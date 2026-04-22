74e8a345b2660ffbaec8522c8b8d7e8b864735bd
Alistair Sinclair-Smith <ajss90@hotmail.co.uk>
2026-04-22 21:07:07 +0100

    adding README to match repo from github

--- obj/Debug/net10.0/ptcg-cataloguer-storage-api.AssemblyInfo.cs
+++ obj/Debug/net10.0/ptcg-cataloguer-storage-api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ptcg-cataloguer-storage-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10617136a64c97487d35960fdea3af9eb06db824")]
 [assembly: System.Reflection.AssemblyProductAttribute("ptcg-cataloguer-storage-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ptcg-cataloguer-storage-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
